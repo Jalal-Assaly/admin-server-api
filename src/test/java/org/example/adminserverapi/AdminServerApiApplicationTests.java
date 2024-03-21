a6a1afefa07b606595908f32ef2996e6806d2a44
Jalal Assaly <jalal.assaly@gmail.com>
2024-03-21 21:49:18 +0200

    Initial Commit

---
+++ src/test/java/org/example/adminserverapi/AdminServerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.adminserverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
